a4e973668a99a5bb6a99f23f69852c7e998c7b16
Mixss <michalziemiec@wp.pl>
2022-11-04 13:13:04 +0100

    disk_header.h: Updated record to meet the specification

--- include/disk_reader.h
+++ include/disk_reader.h
@@ -4,10 +4,13 @@
 
 #define BLOCK_SIZE sizeof(struct block)
 
+/*44. File records: parameters a0,a1,a2,a3,a4,x.
+Sorting by the value of the function g(x)=a0+a1x +a2x2+a3x3+a4x4*/
 struct record 
 {
     int id;
-    long data;
+    int a[5];
+    int x;
 } __attribute__((packed)); 
 
 struct block 
